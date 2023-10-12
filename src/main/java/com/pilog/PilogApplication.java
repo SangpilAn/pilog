0b6f1a14a8d195fd322c3eb0d42c9f2dbcc484ab
asp4303 <asp4303@gmail.com>
2023-10-12 15:02:14 +0900

    Project Start

---
+++ src/main/java/com/pilog/PilogApplication.java
@@ -0,0 +1,13 @@
+package com.pilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilogApplication.class, args);
+    }
+
+}
